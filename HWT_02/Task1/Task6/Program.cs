7f4f50ebf6af844f086440d2a62d68a6e7044c2a
IldarAbdullin <wud_i@mail.ru>
2017-11-30 22:34:17 +0400

    End

--- HWT_02/Task1/Task6/Program.cs
+++ HWT_02/Task1/Task6/Program.cs
@@ -10,64 +10,17 @@ namespace Task6
 
     public class Program
     {
-        enum Params
-        {
-            Bold,
-            Italic,
-            underline
-        }
-
         public static void Main(string[] args)
         {
             string style = "None";
-            bool flag = true;
             bool flagBold, flagItalic, flagUnderline;
             flagBold = flagItalic = flagUnderline = false;
-            PrintMenu(style);
-            int.TryParse(Console.ReadLine(), out int n);
-            Restyle(n, flagBold, flagItalic, flagUnderline);
-
-        }
-
-        public static void Restyle(int n, bool flagBold, bool flagItalic, bool flagUnderline, string  style)
-        {
-            switch (n)
+            while (true)
             {
-                case 1:
-                    if (flagBold)
-                    {
-                        if (style.Contains(", bold"))
-                        {
-                            style = style.Replace(", bold", "");
-                        }
-                        else if ((style.Contains("bold,")))
-                        {
-                            style = style.Replace("bold,", "");
-                        }
-                        else if ((style.Contains("bold")))
-                        {
-                            style = style.Replace("bold", "");
-                        }
-
-                        flagBold = false;
-
-                    }
-                    else
-                    {
-                        flagBold = true;
-                        style = style + ", bold";
-                    }
-
-                    if ((!flagItalic) && (!flagUnderline) && (!flagBold))
-                    {
-                        style = "None";
-                    }
+                Class.PrintMenu(style);
+                int.TryParse(Console.ReadLine(), out int n);
+                Class.Restyle(n, ref flagBold, ref flagItalic, ref flagUnderline, ref style);
             }
-        }
-
-        public static void PrintMenu(string style)
-        {
-            Console.WriteLine("Пармаетр надписи:  {0}\n Введите:\n\t 1:  bold\n\t 2:  italic\n\t 3:  underline",style);
-        }
+        }      
     }
 }
