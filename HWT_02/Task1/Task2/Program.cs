738cd9c04a9d8e57358b4d9cea376af23634318e
IldarAbdullin <wud_i@mail.ru>
2017-11-30 17:34:31 +0400

    New

---
+++ HWT_02/Task1/Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+ Написать программу, которая запрашивает с клавиатуры число N и выводит на экран следующее «изображение», состоящее из N строк:
+ */
+
+namespace Task2
+{
+    using System;
+
+    public class Program2
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number N.");
+            int.TryParse(Console.ReadLine(), out int n);
+            Print1(n, '*');
+            Console.ReadKey();
+        }
+
+        public static void Print1(int n, char symbol)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                int count = i;
+                while (count >= 0)
+                { 
+                    Console.Write("{0}", symbol);
+                    count -= 1;
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ HWT_02/Task1/Task6/Program.cs
@@ -0,0 +1,73 @@
+﻿/*
+ Для выделения текстовой надписи можно использовать выделение жирным, курсивом и подчёркиванием. 
+ Предложите способ хранения информации о выделении надписи и напишите программу, 
+ которая позволяет назначать и удалять текстовой надписи выделение:
+ */
+
+namespace Task6
+{
+    using System;
+
+    public class Program
+    {
+        enum Params
+        {
+            Bold,
+            Italic,
+            underline
+        }
+
+        public static void Main(string[] args)
+        {
+            string style = "None";
+            bool flag = true;
+            bool flagBold, flagItalic, flagUnderline;
+            flagBold = flagItalic = flagUnderline = false;
+            PrintMenu(style);
+            int.TryParse(Console.ReadLine(), out int n);
+            Restyle(n, flagBold, flagItalic, flagUnderline);
+
+        }
+
+        public static void Restyle(int n, bool flagBold, bool flagItalic, bool flagUnderline, string  style)
+        {
+            switch (n)
+            {
+                case 1:
+                    if (flagBold)
+                    {
+                        if (style.Contains(", bold"))
+                        {
+                            style = style.Replace(", bold", "");
+                        }
+                        else if ((style.Contains("bold,")))
+                        {
+                            style = style.Replace("bold,", "");
+                        }
+                        else if ((style.Contains("bold")))
+                        {
+                            style = style.Replace("bold", "");
+                        }
+
+                        flagBold = false;
+
+                    }
+                    else
+                    {
+                        flagBold = true;
+                        style = style + ", bold";
+                    }
+
+                    if ((!flagItalic) && (!flagUnderline) && (!flagBold))
+                    {
+                        style = "None";
+                    }
+            }
+        }
+
+        public static void PrintMenu(string style)
+        {
+            Console.WriteLine("Пармаетр надписи:  {0}\n Введите:\n\t 1:  bold\n\t 2:  italic\n\t 3:  underline",style);
+        }
+    }
+}
