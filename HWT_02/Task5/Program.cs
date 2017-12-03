855221b109188c55e9f1b2a2fc2a8b851db7cb26
IldarAbdullin <wud_i@mail.ru>
2017-12-03 16:10:29 +0400

    HWT3

---
+++ HWT_02/Task5/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ Если выписать все натуральные числа меньше 10, кратные 3, или 5, то получим 3, 5, 6 и 9. Сумма этих чисел будет равна 23.
+ Напишите программу, которая выводит на экран сумму всех чисел меньше 1000, кратных 3, или 5.
+ */
+
+namespace Task5
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int sum = 0;
+            int num = 1;
+            while (sum + num < 1000)
+            { 
+                if ((num % 3 == 0) || (num % 5 == 0))
+                {
+                    sum += num;
+                }
+
+                num++;
+            }
+
+            Console.WriteLine("The sum of the numbers of multiples of 3 or 5 is less than 1000 = {0}", sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HWT_02/Task6/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ Для выделения текстовой надписи можно использовать выделение жирным, курсивом и подчёркиванием. 
+ Предложите способ хранения информации о выделении надписи и напишите программу, 
+ которая позволяет назначать и удалять текстовой надписи выделение:
+ */
+
+namespace Task6
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string style = "None";
+            bool flagBold, flagItalic, flagUnderline;
+            flagBold = flagItalic = flagUnderline = false;
+            while (true)
+            {
+                Class.PrintMenu(style);
+                int.TryParse(Console.ReadLine(), out int n);
+                Class.Restyle(n, ref flagBold, ref flagItalic, ref flagUnderline, ref style);
+                if (n == 0)
+                {
+                    break;
+                }
+            }
+        }      
+    }
+}
