73f6b13cf4f104653e70801f9ddbecbbebbb25fe
IldarAbdullin <wud_i@mail.ru>
2017-12-10 23:42:19 +0400

    доделал 4 домашку

---
+++ HWT_04/Task03/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+ Проведите сравнительный анализ скорости работы классов String и
+StringBuilder для операции сложения строк:
+ */
+namespace Task03
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            TestTimeString.InputAndCalc();
+        }
+    }
+}
---
+++ HWT_04/Task03/TestTimeString.cs
@@ -0,0 +1,63 @@
+﻿namespace Task03
+{
+    using System;
+    using System.Diagnostics;
+    using System.Text;
+
+    public class TestTimeString
+    {
+        public const int NumTests = 4; ////количество тестов
+
+        public static void InputAndCalc()
+        {
+            int flag = 1;
+            while (flag == 1)
+            {
+                Console.WriteLine("Enter number of steps:\n");
+                int.TryParse(Console.ReadLine(), out int n);
+                for (int i = 0; i < NumTests; i++)
+                {
+                    CheckString(n);
+                    CheckStringBuilder(n);
+                    n = n * 10;
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("If you want to enter a new line, enter 1, otherwise 0:\n");
+                int.TryParse(Console.ReadLine(), out flag);
+                if (flag == 1)
+                {
+                    Console.Clear();
+                }
+            }
+        }
+
+        public static void CheckString(int n)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            string str = string.Empty;
+            for (int i = 0; i < n; i++)
+            {
+                str += "*";
+            }
+
+            sw.Stop();
+            Console.WriteLine($"Operating time string after {n} steps:\n" + sw.Elapsed);
+        }
+
+        public static void CheckStringBuilder(int n)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            StringBuilder str = new StringBuilder();
+            for (int i = 0; i < n; i++)
+            {
+                str.Append("*");
+            }
+
+            sw.Stop();
+            Console.WriteLine($"Operating time StringBuilder after {n} steps:\n" + sw.Elapsed);
+        }
+    }
+}
